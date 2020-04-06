9e872fbe3cb044547ddb91966fe3f911671db8fc
Veer <agrawalv@asbindia.org>
2020-04-06 11:54:25 +0530

    Update covid19.py

--- Covid19Project/covid19.py
+++ Covid19Project/covid19.py
@@ -30,3 +30,22 @@ for row in all_rows:
     if len(stat) == 5: 
         stats.append(stat)
 
+#Labeling the table and using the library pandas for processing it 
+new_cols = ["no.", "States/UT","Confirmed","Recovered","Deceased"]
+state_data = pd.DataFrame(data = stats, columns = new_cols)
+
+#The data is in strings so converting it to Integers
+state_data['Confirmed'] = state_data['Confirmed'].map(int)
+state_data['Recovered'] = state_data['Recovered'].map(int)
+state_data['Deceased']  = state_data['Deceased'].map(int)
+
+#Using the library Prety table to represent the data
+table = PrettyTable()
+table.field_names = (new_cols)
+for i in stats:
+    table.add_row(i)
+table.add_row(["","Total", 
+               sum(state_data['Confirmed']), 
+               sum(state_data['Recovered']), 
+               sum(state_data['Deceased'])])
+print(table)
